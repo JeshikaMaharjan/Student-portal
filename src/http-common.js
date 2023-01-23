728e5e66b2cb1e7290432d2cd9f8fc88522b763f
Jeshika Maharjan <jeshika99@gmail.com>
2023-01-23 20:35:28 +0545

    axios config file

--- src/http-common.js
+++ src/http-common.js
@@ -1,8 +1,24 @@
 import axios from "axios";
 
-export default axios.create({
-  baseURL: "https://minor-production.up.railway.app/api",
-  headers: {
-    "Content-type": "application/json"
-  }
-});
+const baseURL = 'http://192.168.101.81:8000/api';
+
+const basicInstance = axios.create(
+    {
+        baseURL : baseURL,
+        timeout : 5000    
+    }
+);
+
+const tokenInstance = axios.create(
+    {
+        baseURL : baseURL,
+        timeout : 5000,
+        headers : {
+            'Authorization' : `Bearer ${localStorage.getItem('access_token')}`,
+            'Content-Type' : 'application/json'
+        }    
+    }
+);
+export  {basicInstance,tokenInstance};
+  
+
