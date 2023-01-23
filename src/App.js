fdf4a927d4c73bbd14500cb28349a1bea51a49fa
Jeshika Maharjan <jeshika99@gmail.com>
2023-01-23 20:35:09 +0545

    Main app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import {Route,Routes} from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import './css/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Profile } from './components/Admin/Profile';
-import {Update} from './components/Admin/Update';
+import Login from './components/Login';
+import Profile from './components/Admin/Profile';
+import Update from './components/Admin/Update';
 import Register from './components/Admin/Register';
 import Users from './components/Users';
 import UserDetails from './components/UserDetails';
@@ -11,28 +12,56 @@ import RegisterStaff from './components/Admin/RegisterStaff';
 import Search from './components/Admin/Search';
 import { NoMatch } from './components/NoMatch';
 
+import Khalti from './components/Khalti/Khalti';
+import Home from './components/Admin/Home';
+import SideNav from './components/Sidenav';
+import { useState } from 'react';
+import RegisterRoutes from './components/Admin/RegisterRoutes';
+
 function App() {
-  return (
-    <>      
+  // const role = 0; 
+  // setRole(role);
+  // function setRole(newrole){
+  //   localStorage.setItem('role', newrole);
+  // }
+  // assignedrole = localStorage.getItem('role');
+
+  // if (assignedrole == 0){
+  //   return(
+  //     <>
+  //     <SideNav/>        
+  //     <Routes>
+  //       <Route path ='/' element = {<Login/>}/>
+  //       <Route path ='/login' element = {<Login/>}/>
+  //       <Route path='*' element ={<NoMatch/>}/>
+  //     </Routes>
+
+  //     </>
+  //   )
+  // }
+
+  return ( 
+    <>
+      <SideNav/>        
       <Routes>
-        <Route path ='/' element = {<Profile/>}/>
-        <Route path ='profile' element = {<Profile/>}/>
+        <Route path ='/' element = {<Login/>}/>
+        <Route path ='/login' element = {<Login/>}/>
+        <Route path ='home' element = {<Home/>}>
+          <Route path ='profile' element = {<Profile/>}/>
 
-        <Route path ='registration' element = {<Register/>}>
-          <Route index element = {<RegisterStudent/>}/>
-          <Route path ='registerstudent' element = {<RegisterStudent/>}/>
-          <Route path ='registerstaff' element = {<RegisterStaff />}/>
-        </Route>
 
-        <Route path ='update' element = {<Update/>}/>
-        <Route path ='search' element = {<Search />}/>
+          <Route path ='registration/*' element = {<RegisterRoutes/>}/> 
+          <Route path ='update' element = {<Update/>}/>
+          <Route path ='search' element = {<Search />}/>
+        </Route> 
+
         <Route path ='users' element = {<Users/>}/> 
         <Route path='userId' element= {<UserDetails/>}/> 
+        <Route path ='payment' element = {<Khalti/>}/>
         <Route path='*' element ={<NoMatch/>}/>
-      </Routes>      
-    </>
-  
+      </Routes> 
+    </>      
   );
-}
+}  
 
 export default App;
