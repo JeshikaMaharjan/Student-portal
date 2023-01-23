f7b1f0ce58e66e193fd89a36a410df4831e39ae9
Jeshika Maharjan <jeshika99@gmail.com>
2023-01-23 20:25:43 +0545

    Common layout

---
+++ src/components/Admin/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Dashboard from './Dashboard';
+import Sidenav from '../Sidenav';
+
+const Home = () => {
+
+    return (
+        <>
+            {/* <Sidenav /> */}
+            <Dashboard />
+        </>
+
+    );
+}
+
+export default Home;
