1402b8065b6ca62f940b3d8145a61004fe299958
Jeshika Maharjan <jeshika99@gmail.com>
2023-01-24 19:51:39 +0545

    Main App

--- src/App.js
+++ src/App.js
@@ -2,58 +2,47 @@ import { Route, Routes } from 'react-router-dom';
 import './css/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './components/Login';
-import Profile from './components/Admin/Profile';
-import Update from './components/Admin/Update';
-import Register from './components/Admin/Register';
 import Users from './components/Users';
 import UserDetails from './components/UserDetails';
-import RegisterStudent from './components/Admin/RegisterStudent';
-import RegisterStaff from './components/Admin/RegisterStaff';
-import Search from './components/Admin/Search';
+import AdminRoutes from './components/Routes/AdminRoutes';
 import { NoMatch } from './components/NoMatch';
-
 import Khalti from './components/Khalti/Khalti';
-import Home from './components/Admin/Home';
-import SideNav from './components/Sidenav';
-import { useState } from 'react';
-import RegisterRoutes from './components/Admin/RegisterRoutes';
+import Unauthorized from './components/Unauthorized';
+import RequireAuth from './components/RequireAuth';
+import AccountRoutes from './components/Routes/Account_staffRoutes';
+import StudentRoutes from './components/Routes/StudentRoutes';
 
+
+
+const ROLES = {
+  'Admin' : 1,
+  'Staff': 2,
+  'Student':3
+}
 function App() {
-  // const role = 0; 
-  // setRole(role);
-  // function setRole(newrole){
-  //   localStorage.setItem('role', newrole);
-  // }
-  // assignedrole = localStorage.getItem('role');
-
-  // if (assignedrole == 0){
-  //   return(
-  //     <>
-  //     <SideNav/>        
-  //     <Routes>
-  //       <Route path ='/' element = {<Login/>}/>
-  //       <Route path ='/login' element = {<Login/>}/>
-  //       <Route path='*' element ={<NoMatch/>}/>
-  //     </Routes>
-
-  //     </>
-  //   )
-  // }
-
   return ( 
     <>
-      <SideNav/>        
       <Routes>
+        {/* public routes */}
         <Route path ='/' element = {<Login/>}/>
         <Route path ='/login' element = {<Login/>}/>
-        <Route path ='home' element = {<Home/>}>
-          <Route path ='profile' element = {<Profile/>}/>
+        <Route path="unauthorized" element={<Unauthorized />} />
+        <Route path="accountstaff/*" element={<AccountRoutes />} />          
+        <Route path="student/*" element={<StudentRoutes />} />          
+        <Route path="admin/*" element={<AdminRoutes />} />          
 
+        {/*  protected routes */}
+        {/* <Route element={<RequireAuth allowedRoles={[ROLES.Admin]} />}>
+          <Route path="admin/*" element={<AdminRoutes />} />          
+        </Route>  
 
-          <Route path ='registration/*' element = {<RegisterRoutes/>}/> 
-          <Route path ='update' element = {<Update/>}/>
-          <Route path ='search' element = {<Search />}/>
+        <Route element={<RequireAuth allowedRoles={[ROLES.Staff]} />}>
+          <Route path="accountstaff/*" element={<AccountRoutes />} />          
         </Route> 
+
+        <Route element={<RequireAuth allowedRoles={[ROLES.Student]} />}>
+          <Route path="student/*" element={<StudentRoutes />} />          
+        </Route>         */}
 
         <Route path ='users' element = {<Users/>}/> 
         <Route path='userId' element= {<UserDetails/>}/> 
@@ -63,5 +52,28 @@ function App() {
     </>      
   );
 }  
+//   return ( 
+//     <>
+//       <SideNav/>        
+//       <Routes>
+//         <Route path ='/' element = {<Login/>}/>
+//         <Route path ='/login' element = {<Login/>}/>
+//         <Route path ='home' element = {<Home/>}>
+//           <Route path ='profile' element = {<Profile/>}/>
+
+
+//           <Route path ='registration/*' element = {<RegisterRoutes/>}/> 
+//           <Route path ='update' element = {<Update/>}/>
+//           <Route path ='search' element = {<Search />}/>
+//         </Route> 
+
+//         <Route path ='users' element = {<Users/>}/> 
+//         <Route path='userId' element= {<UserDetails/>}/> 
+//         <Route path ='payment' element = {<Khalti/>}/>
+//         <Route path='*' element ={<NoMatch/>}/>
+//       </Routes> 
+//     </>      
+//   );
+// }  
 
 export default App;
